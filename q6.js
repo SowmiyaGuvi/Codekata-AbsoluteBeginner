87f9b8b518ee91885744c8b374a262953d896435
SowmiyaGuvi <sowmimiya43@gmail.com>
2024-03-02 03:58:42 +0530

    q6 file commit

---
+++ q6.js
@@ -0,0 +1,28 @@
+//You are provided with two numbers. Find and print the smaller number.
+
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+
+  const strArr = userInput[0].split(" ");
+  
+  const a = +strArr[0];
+  const b = +strArr[1];
+  if(a < b) console.log(a);
+  else console.log(b);
+
+  //end-here
+});
