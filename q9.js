64b6824847d340fb24da8a7b264c0cceb4a23605
SowmiyaGuvi <sowmimiya43@gmail.com>
2024-03-02 04:02:21 +0530

    q8 file commit

---
+++ q9.js
@@ -0,0 +1,23 @@
+//You are given with a number "N", find its cube.
+
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+
+const num = parseInt(userInput[0])
+  console.log(num * num *num);
+  //end-here
+});
